a014f083898d0a8fac949dfdabd284d833910e66
Cesar Baez <baezcesar329@gmail.com>
2022-09-19 13:28:41 -0400

    Creación de archivo createProduct.ejs y controlador y ruta para esa vista

--- Controllers/productsController.js
+++ Controllers/productsController.js
@@ -1,5 +1,11 @@
 module.exports = {
   index:function(req, res){
     res.render('products/index', {title: 'Productos'});
+  },
+
+  createProduct:function(req, res){
+    res.render('products/createProduct', {
+      title: 'Crear producto'
+    });
   }
 }
--- routes/products.js
+++ routes/products.js
@@ -4,5 +4,6 @@ var router = express.Router();
 const productsController = require('../Controllers/productsController');
 
 router.get('/', productsController.index);
+router.get('/createProduct', productsController.createProduct);
 
 module.exports = router;

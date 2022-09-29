e42bd8d7a08e5c4951749801cae15d714c8f3fab
Cesar Baez <baezcesar329@gmail.com>
2022-09-29 14:55:53 -0400

    Creación de vista createMaterial

--- Controllers/materialsController.js
+++ Controllers/materialsController.js
@@ -1,5 +1,11 @@
 module.exports = {
   index:function(req, res){
     res.render('materials/index', {title: 'Materiales'});
+  },
+
+  createMaterial:function(req, res){
+    res.render('materials/createMaterial', {
+      title: 'Registrar material'
+    });
   }
 }
--- routes/materials.js
+++ routes/materials.js
@@ -4,5 +4,6 @@ var router = express.Router();
 const materialsRouter = require('../Controllers/materialsController');
 
 router.get('/', materialsRouter.index);
+router.get('/createMaterial', materialsRouter.createMaterial);
 
 module.exports = router;

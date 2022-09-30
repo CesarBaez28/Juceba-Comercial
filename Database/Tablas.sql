7d40e0ef2c26abd89fd7a9737e5e21c40844536f
Cesar Baez <baezcesar329@gmail.com>
2022-09-30 14:36:33 -0400

    Creación de las tablas usuarios y tipo de usuarios en la base de datos

---
+++ Database/Tablas.sql
@@ -0,0 +1,22 @@
+/*Tipo de usuarios: empleado, administrador...*/
+CREATE TABLE tipo_usuarios(
+  codigo int auto_increment, /*pk*/
+  constraint pk_codigo_tipo_usuarios primary key(codigo),
+  tipo_usuario varchar(100) NOT NULL UNIQUE,
+  descripcion varchar(250) DEFAULT '',
+  estado bit default 1
+);
+
+/*Usuarios del sistema*/
+create table usuarios(
+  codigo int auto_increment, /*pk*/
+  constraint pk_codigo_usuarios primary key(codigo),
+  codigo_tipo_usuario int not null, /*fk*/
+  constraint fk_codigo_tipo_usuarios_usuarios foreign key (codigo_tipo_usuario) references tipo_usuarios (codigo),
+  nombre_usuario varchar(100) not null unique,
+  nombre varchar(100) not null /*Nombre de la persona*/,
+  paswd varchar(250) not null,
+  foto varchar(250) default '',
+  descripcion varchar(250) default '',
+  estado bit default 1
+);

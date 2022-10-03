0c141faf0d2cc5e75bf9047479e6373c715693b5
Cesar Baez <baezcesar329@gmail.com>
2022-10-03 11:55:48 -0400

    Creación de las tablas,provincias, municipios, sectores, calles_y_numero, direcciones y compañías en la base de datos

--- Database/Tablas.sql
+++ Database/Tablas.sql
@@ -1,3 +1,69 @@
+/*Calles y número de calles*/
+CREATE TABLE callesYnumero (
+  codigo int auto_increment /*pk*/,
+  constraint pk_codigo_calleYnumero primary key (codigo),
+  calle_y_numero varchar (100) not null unique,
+  descripcion varchar (250),
+  estado bit default 1
+);
+
+/*Sectores*/
+CREATE TABLE sectores (
+  codigo int auto_increment /*pk*/,
+  constraint pk_codigo_sector primary key (codigo),
+  sector varchar (100) not null unique,
+  descripcion varchar (250),
+  estado bit default 1
+);
+
+/*Municipios*/
+CREATE TABLE municipios (
+  codigo int auto_increment /*pk*/,
+  constraint pk_codigo_municipio primary key (codigo),
+  municipio varchar (50) not null unique,
+  descripcion varchar (250),
+  estado bit default 1
+);
+
+/*Provincias*/
+CREATE TABLE provincias (
+  codigo int auto_increment /*pk*/,
+  constraint pk_codigo_provincia primary key (codigo),
+  provincia varchar (50) not null unique,
+  descripcion varchar (250),
+  estado bit default 1
+);
+
+/*Direcciones*/
+CREATE TABLE  direcciones (
+  codigo int auto_increment unique,
+  codigo_calle_y_numero int not null,
+  constraint fk_codigo_calle_y_numero_direccion foreign key (codigo_calle_y_numero) references callesYnumero(codigo),
+  codigo_sector int not null,
+  constraint fk_codigo_sector_direccion foreign key (codigo_sector) references sectores(codigo),
+  codigo_municipio int not null,
+  constraint fk_codigo_municipio_direccion foreign key (codigo_municipio) references municipios (codigo),
+  codigo_provincia int not null,
+  constraint fk_codigo_provincia_direccion foreign key (codigo_provincia) references provincias (codigo),
+  constraint cc_codigo_direccion primary key (codigo_calle_y_numero, codigo_sector, codigo_municipio, codigo_provincia),
+  descripcion varchar (250),
+  estado bit default 1
+);
+
+/*Compañías, negocios o empresas*/
+CREATE TABLE empresas (
+  codigo int auto_increment /*pk*/,
+  constraint fk_codigo_empresa primary key (codigo),
+  codigo_telefono int not null,
+  constraint fk_codigo_telefono_empresa foreign key (codigo_telefono) references telefonos (codigo),
+  codigo_direccion int not null,
+  constraint fk_codigo_direccion_empresa foreign key (codigo_direccion) references direcciones (codigo),
+  nombre varchar (100),
+  email varchar (100),
+  descripcion varchar (250),
+  estado bit default 1
+);
+
 /*Números de teléfonos*/
 CREATE TABLE telefonos (
   codigo int auto_increment /*pk*/,
@@ -32,9 +98,3 @@ create table usuarios(
   descripcion varchar(250) default '',
   estado bit default 1
 );
-
-Select usuarios.codigo, tipo_usuarios.tipo_usuario, usuarios.nombre_usuario, 
-  usuarios.nombre, telefonos.telefono, usuarios.email, 
-  CASE WHEN usuarios.estado = 1 Then 'Activo' ELSE 'Inactivo' END AS estado
-  FROM usuarios join tipo_usuarios on usuarios.codigo_tipo_usuario = tipo_usuarios.codigo
-  join telefonos on usuarios.codigo_telefono = telefonos.codigo;

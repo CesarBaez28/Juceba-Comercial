bf7e28fd9abcee105d6c57febd539fd70b5e731f
Cesar Baez <baezcesar329@gmail.com>
2022-10-15 15:14:17 -0400

    Correción de error en archivos duplicados de estilos en cascadas

--- routes/clients.js
+++ routes/clients.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const {isLoggedIn} = require('../Config/auth');
-
-
 const clientsController = require('../Controllers/clientsController');
 
 router.get('/', isLoggedIn, clientsController.index);

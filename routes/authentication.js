caddcd40917b7fcd20cee718318aa8073e9ba913
Cesar Baez <baezcesar329@gmail.com>
2022-10-14 23:07:33 -0400

    Programación para proteger vistas luego del inicio de sesión

--- routes/authentication.js
+++ routes/authentication.js
@@ -2,8 +2,9 @@ const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 const authenticationController = require("../Controllers/authenticationController");
+const {isLoggedIn, isNotLoggedIn} = require('../Config/auth');
 
-router.get('/login', authenticationController.login);
+router.get('/login', isNotLoggedIn, authenticationController.login);
 router.post('/login', authenticationController.authenticate);
 router.get('/createAccount', authenticationController.createAccount);
 router.post('/createAccount', authenticationController.signup);

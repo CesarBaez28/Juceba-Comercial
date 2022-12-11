61b89fb822d79861d0c3be4d2d009cee2bdea638
Cesar Baez <baezcesar329@gmail.com>
2022-12-10 23:49:00 -0400

    Correción de error en las variables de entorno

--- Config/config.js
+++ Config/config.js
@@ -3,6 +3,6 @@ module.exports = {
   DB_HOST: process.env.DB_HOST || 'localhost',
   DB_USER: process.env.DB_USER || 'root',
   DB_PASSWORD: process.env.DB_PASSWORD || 'las70semanas',
-  DB_NAME: process.env.DB_HOST || 'JucebaComercial',
-  DB_PORT: process.env.DB_HOST || 3306
+  DB_NAME: process.env.DB_NAME || 'JucebaComercial',
+  DB_PORT: process.env.DB_PORT || 3306
 }
